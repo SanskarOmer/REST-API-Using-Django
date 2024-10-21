76897d6243a506c8d7acb5124ef19da64ab8f2ad
SanskarOmer <sanskaromer7@gmail.com>
2024-10-22 00:12:12 +0530

    added a new serializer

--- DRFProject/DRFProject/views.py
+++ DRFProject/DRFProject/views.py
@@ -13,8 +13,9 @@ class TestView(APIView):
         }
         return Response(data)
     
-    def post(self ,request ,*args,*kwargs):
+    def post(self ,request ,*args,**kwargs):
         serializer = StudentSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        return Response(serializer.error)
