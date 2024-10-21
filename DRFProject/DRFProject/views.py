2c20c35f30c53a63b560e03e53d76eaa765082d3
SanskarOmer <sanskaromer7@gmail.com>
2024-10-22 00:10:25 +0530

    added a new serializer

--- DRFProject/DRFProject/views.py
+++ DRFProject/DRFProject/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .serializers import StudentSerializer
+from .models import Student
+
 class TestView(APIView):
     def get(self, request, *args ,**kwargs):
         data={
@@ -9,3 +12,9 @@ class TestView(APIView):
             'years_active':10
         }
         return Response(data)
+    
+    def post(self ,request ,*args,*kwargs):
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
