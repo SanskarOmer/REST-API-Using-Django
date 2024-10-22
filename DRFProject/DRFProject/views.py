cf600f70b24ba875394e28316173fca28bf311da
SanskarOmer <sanskaromer7@gmail.com>
2024-10-22 23:48:55 +0530

    added new get functionality

--- DRFProject/DRFProject/views.py
+++ DRFProject/DRFProject/views.py
@@ -7,11 +7,9 @@ from DRFapp.models import Student
 
 class TestView(APIView):
     def get(self, request, *args ,**kwargs):
-        data={
-            'username':'admin',
-            'years_active':10
-        }
-        return Response(data)
+        qs = Student.objects.all()
+        serializer = StudentSerializer(qs , many=True)
+        return Response(serializer.data)
     
     def post(self ,request ,*args,**kwargs):
         serializer = StudentSerializer(data = request.data)
